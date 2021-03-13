d87ac234a6d11dbc8a1d1f126593e5b79fa45376
yjsong2154 <yjsong2153@gmail.com>
2021-03-14 03:10:44 +0900

    picker event

--- component/TimerSetup.js
+++ component/TimerSetup.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import { Dimensions, View, Text, Button } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { Dimensions, View, Text, StyleSheet, Button, Alert } from 'react-native';
 import WheelPicker from './WheelPicker';
 
 const { width, height } = Dimensions.get('window');
+const BACK_COLOR = 'white';
 
-export default class TimerSetup extends React.Component {
-	state = {
-		items: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
-		selectedItem: 1
-	};
+export default function TimerSetup() {
+	const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+	const [ number, setNumber ] = useState(1);
+	const works = [ 15, 20, 25, 30, 35, 40, 45, 50, 55, 60 ];
+	const [ work, setWork ] = useState(25);
+	const noworks = [ 3, 5, 10, 15, 20 ];
+	const [ nowork, setNowork ] = useState(5);
 
-	render() {
-		console.log(height);
-		return (
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				<View style={{ height: 400 }}>
-					<WheelPicker items={this.state.items} selected={this.state.selectedItem} />
-				</View>
-				<Button style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} title="Go to Details" />
-			</View>
-		);
-	}
+	const onPress = React.useCallback(() => {
+		Alert.alert(number + ' ' + work + ' ' + nowork);
+	});
+
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>pomodoro</Text>
+			<WheelPicker style={styles.wheel} items={numbers} onChange={setNumber} selected={0} contentStyle={itemStyle} />
+			<WheelPicker style={styles.wheel} items={works} onChange={setWork} selected={2} contentStyle={itemStyle} />
+			<WheelPicker style={styles.wheel} items={noworks} onChange={setNowork} selected={1} contentStyle={itemStyle} />
+			<Button style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} onPress={onPress} title="START" />
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	wheel: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: 300
+	}
+});
+
+const itemStyle = {
+	FONT_SIZE: 30,
+	ITEM_SIZE: 40
+};
--- component/WheelPicker.js
+++ component/WheelPicker.js
@@ -1,47 +1,55 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, Dimensions, FlatList } from 'react-native';
-
-const ICON_SIZE = 42;
-const ITEM_HEIGHT = ICON_SIZE * 2;
-const { width, height } = Dimensions.get('window');
+import { View, Text, FlatList } from 'react-native';
 
 function WheelPicker(props) {
 	const item = props.items;
-	console.log({ item });
+	const contentStyle = props.contentStyle;
+	const FONT_SIZE = contentStyle.FONT_SIZE;
+	const ITEM_SIZE = contentStyle.ITEM_SIZE;
+	console.log(props.selected);
+
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+		<View style={[ props.style, { height: ITEM_SIZE * 3 } ]}>
 			<FlatList
 				showsVerticalScrollIndicator={false}
 				showsHorizontalScrollIndicator={false}
+				snapToInterval={40}
+				decelerationRate="fast"
 				contentContainerStyle={{
-					paddingTop: 200,
-					paddingBottom: 200
+					paddingTop: ITEM_SIZE,
+					paddingBottom: ITEM_SIZE
 				}}
+				initialScrollIndex={props.selected}
+				onMomentumScrollEnd={(e) => {
+					const newValue = Math.round(e.nativeEvent.contentOffset.y / ITEM_SIZE);
+					if (props.onChange) {
+						props.onChange(item[newValue]);
+					}
+				}}
+				getItemLayout={(data, index) => ({ length: ITEM_SIZE, offset: ITEM_SIZE * index, index })}
 				keyExtractor={(item) => item.toString()}
 				data={item}
-				renderItem={({ item }) => <Item num={item} />}
+				renderItem={({ item }) => (
+					<View
+						style={{
+							height: ITEM_SIZE,
+							width: props.style.width
+						}}
+					>
+						<Text
+							style={{
+								textAlign: 'center',
+								textAlignVertical: 'center',
+								fontSize: FONT_SIZE
+							}}
+						>
+							{item}
+						</Text>
+					</View>
+				)}
 			/>
 		</View>
 	);
 }
-
-const Item = ({ num }) => {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.text}>{num}</Text>
-		</View>
-	);
-};
-
-const styles = StyleSheet.create({
-	container: {
-		height: 40
-	},
-	text: {
-		textAlign: 'center',
-		textAlignVertical: 'center',
-		fontSize: 30
-	}
-});
 
 export default WheelPicker;
